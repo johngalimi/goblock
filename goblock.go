d1a2a46f8b9b3751595260f37059ac52fff2b73a
johngalimi <galimijohn@gmail.com>
2019-12-18 22:53:16 -0500

    added hash and conversion for structs

--- goblock.go
+++ goblock.go
@@ -12,28 +12,26 @@ type BlockContents struct {
 	TransactionCount int `json:"transactionCount"`
 }
 
-func hash(s string) uint32 {
+func hashBlock(s string) uint32 {
 	h := fnv.New32a()
 	h.Write([]byte(s))
 	return h.Sum32()
 }
 
-func convert() string {
-	bytes, err := json.Marshal(BlockContents{
-		BlockNumber: 1,
-		ParentHash: 123456789,
-		TransactionCount: 7,
-	})
+func convertBlock(block BlockContents) string {
+	bytes, err := json.Marshal(block)
 	if err != nil {
 		panic(err)
 	}
-
 	return string(bytes)
 }
 
 func main() {
-	testconv := convert()
-	fmt.Println(testconv)
-	fmt.Printf("%T\n", testconv)
-	fmt.Println(hash(testconv))
+	testBlock :=  BlockContents{
+		BlockNumber: 1,
+		ParentHash: 12345,
+		TransactionCount: 7,
+	}
+	hashedblock := hashBlock(convertBlock(testBlock))
+	fmt.Println(hashedblock)
 }
