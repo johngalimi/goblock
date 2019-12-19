724809ade4a6b6c704e2aa8c7aef782ab988c964
johngalimi <galimijohn@gmail.com>
2019-12-18 21:54:32 -0500

    convert method on structs

--- goblock.go
+++ goblock.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hash/fnv"
+	"encoding/json"
+)
 
-func add(a int, b int) (int, string) {
-	c := a + b
-	return c, "successfully added"
+type BlockContents struct {
+	BlockNumber int `json:"blockNumber"`
+	ParentHash uint32 `json:"parentHash"`
+	TransactionCount int `json:"transactionCount"`
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
+
+func convert() string {
+	bytes, err := json.Marshal(BlockContents{
+		BlockNumber: 1,
+		ParentHash: 123456789,
+		TransactionCount: 7,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
 }
 
 func main() {
-	sum, message := add(2, 1)
-	fmt.Println(message)
-	fmt.Println(sum)
+	testconv := convert()
+	fmt.Println(testconv)
+	fmt.Printf("%T\n", testconv)
+	fmt.Println(hash(testconv))
 }
